22af89445f2dad342a57c0e29fc4025537c342ed
Dimar Tamara <dimartamara1@gmail.com>
2026-03-10 21:00:46 +0700

    menambahkan backend untuk menambah dan mendapatkan artikel

--- app/admin/articles/page.tsx
+++ app/admin/articles/page.tsx
@@ -4,34 +4,44 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export type Article = {
-  article_id: string
-  title: string
-  cover_url: string
-  content: string
-  created_at: string
-  updated_at: string
+    article_id: string
+    title: string
+    cover_url: string
+    content: string
+    is_recommended: boolean
+    created_at: string
+    updated_at: string
+}
+
+type ResponseType = {
+    success: boolean,
+    message: string,
+    data: Article[]
 }
 
 export default function ArticesPage() {
     const router = useRouter();
-    const [openMenu, setOpenMenu] = useState<null | number>(null);
-    const articles = [
-        {
-            id: 1,
-            title: "First Article",
-            image: "https://placehold.co/800?text=article1&font=roboto",
-        },
-        {
-            id: 2,
-            title: "Second Article",
-            image: "https://placehold.co/800?text=article2&font=roboto",
-        },
-        {
-            id: 3,
-            title: "Third Article",
-            image: "https://placehold.co/800?text=article3&font=roboto",
-        },
-    ];
+    const [openMenu, setOpenMenu] = useState<null | string>(null);
+    const [articles, setArticles] = useState<Article[]>([]);
+
+    useEffect(() => {
+        const getArticles = async () => {
+            try {
+                const responseGet = await fetch(`/api/admin/get-articles`, {
+                    method: 'GET',
+                })
+
+                const res: ResponseType = await responseGet.json()
+                if (res.success) {
+                    setArticles(res.data)
+                }
+            } catch (err) {
+                console.error("Failed to fetch articles:", err)
+            }
+        }
+
+        getArticles()
+    }, [])
 
     const handleCreate = () => {
         router.push("/admin/articles/create");
@@ -41,9 +51,9 @@ export default function ArticesPage() {
         <div className="flex min-h-screen w-full  items-center justify-center bg-gray-50 px-4 sm:px-6 lg:px-40 py-20">
             <div className="relative w-full max-md:w-full h-125 flex justify-start items-center  flex-col gap-y-10">
                 <div className="absolute  right-0 top-0 flex flex-col gap-y-2">
-                    <button 
+                    <button
                         className="rounded-full bg-green1 w-20 h-20 max-md:w-10 max-md:h-10 flex justify-center items-center transition-transform duration-200 hover:scale-104"
-                        onClick={handleCreate}    
+                        onClick={handleCreate}
                     >
                         <span className="text-[80px] max-md:text-[20px] font-light">+</span>
                     </button>
@@ -57,22 +67,22 @@ export default function ArticesPage() {
                 </h1>
 
                 <div className="relative w-full h-auto flex justify-center items-center flex-wrap flex-row gap-x-20 gap-y-5 mt-5">
-                    {articles.map((article) => (
-                        <div key={article.id} className="w-[260px]">
+                    {articles?.map((article) => (
+                        <div key={article.article_id} className="w-[260px]">
                             <div
                                 className="relative h-[180px] rounded-xl overflow-hidden bg-gray-300 bg-cover bg-center"
-                                style={{ backgroundImage: `url(${article.image})` }}
+                                style={{ backgroundImage: `url(${article.cover_url})` }}
                             >
                                 <button
                                     onClick={() =>
-                                        setOpenMenu(openMenu === article.id ? null : article.id)
+                                        setOpenMenu(openMenu === article.article_id ? null : article.article_id)
                                     }
                                     className="absolute top-3 right-3 bg-gray-400 rounded-full px-2 py-1"
                                 >
                                     ⋮
                                 </button>
 
-                                {openMenu === article.id && (
+                                {openMenu === article.article_id && (
                                     <div className="absolute top-10 right-3 bg-gray-500 shadow-lg rounded-lg text-sm">
                                         <button className="block px-4 py-2 hover:bg-gray-600 hover:rounded-lg w-full text-left">
                                             Set as Recommendation
